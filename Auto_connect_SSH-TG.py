d5f3eca2b403a18793e74cb87653bff57fb99615
aoguai <34203474+aoguai@users.noreply.github.com>
2024-09-14 21:51:24 +0800

    feat: Update Auto_connect_SSH-TG.py

--- Auto_connect_SSH-TG.py
+++ Auto_connect_SSH-TG.py
@@ -7,24 +7,10 @@ from datetime import datetime
 import pytz
 from telegram import Bot
 import asyncio
+from telegram.error import TelegramError
 
-# # Telegram Bot 设置
-# BOT_TOKEN = "YOUR_BOT_TOKEN"
-# CHAT_ID = "YOUR_CHAT_ID"
-
-# # 预先定义的常量
-# url = '你检测的地址，参考下一行注释'
-# # 测试URL 这个URL是个凉了的 url = 'https://edwgiz.serv00.net/'
-# ssh_info = {
-#     'host': 's3.serv00.com',    # 主机地址
-#     'port': 22,
-#     'username': '你的用户名',       # 你的用户名，别写错了
-#     'password': '你的SSH密码'       # 你注册的时候收到的密码或者你自己改了的密码
-# }
-
-# 脚本获取的常量  
+# 获取环境变量
 url = os.environ.get('URL')  
-
 hostname = os.environ.get('HOSTNAME')
 ssh_password = os.environ.get('SSH_PASSWORD')
 username = os.environ.get('USERNAME')
@@ -32,8 +18,8 @@ username = os.environ.get('USERNAME')
 ssh_info = {  
     'host': hostname,    # 主机地址
     'port': 22,  
-    'username': username,       # 你的用户名，别写错了
-    'password': ssh_password      # 你注册的时候收到的密码或者你自己改了的密码
+    'username': username,       
+    'password': ssh_password     
 }
 
 # Telegram Bot 设置
@@ -42,8 +28,6 @@ CHAT_ID = os.environ.get('CHAT_ID')
 
 # 获取当前脚本文件的绝对路径
 script_dir = os.path.dirname(os.path.abspath(__file__))
-
-# 日志文件将保存在脚本所在的目录中
 log_file_path = os.path.join(script_dir, 'Auto_connect_SSH.log')
 tg_message_sent = False
 flush_log_message = []
@@ -79,7 +63,7 @@ async def send_telegram_message_async(message):
         await bot.send_message(chat_id=CHAT_ID, text=message)
         tg_status = "Telegram提醒消息发送成功"
         print("温馨提醒：Telegram提醒消息发送成功。")
-    except Exception as e:
+    except TelegramError as e:
         tg_status = f"Telegram提醒消息发送失败，错误码: {e}"
         print(f"警告：Telegram提醒消息发送失败！\n错误码: {e}")
     finally:
@@ -91,46 +75,44 @@ async def send_telegram_message_async(message):
 def send_telegram_message(message):
     asyncio.run(send_telegram_message_async(message))
 
-# 尝试通过SSH恢复PM2进程的函数
-def restore_pm2_processes():
-    try:
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(**ssh_info)
-        
-        stdin, stdout, stderr = ssh.exec_command('pm2 resurrect')
-        output = stdout.read().decode()
-        error = stderr.read().decode()
-        
-        if error:
-            write_log(f"PM2恢复失败: {error}")
-            print(f"PM2恢复失败: {error}")
-        else:
-            write_log(f"PM2恢复成功: {output}")
-            print(f"PM2恢复成功: {output}")
-        
-        ssh.close()
-    except Exception as e:
-        write_log(f"SSH连接失败: {str(e)}")
-        print(f"SSH连接失败: {str(e)}")
+# 尝试通过SSH恢复PM2进程的函数  
+def restore_pm2_processes():  
+    transport = paramiko.Transport((ssh_info['host'], ssh_info['port']))  
+    try:  
+        transport.connect(username=ssh_info['username'], password=ssh_info['password'])  
+        ssh = paramiko.SSHClient()  
+        ssh._transport = transport  
+        try:    # 执行pm2 resurrect命令
+            stdin, stdout, stderr = ssh.exec_command(f'/home/{username}/.npm-global/bin/pm2 resurrect')  
+            print("STDOUT: ", stdout.read().decode())  
+            print("STDERR: ", stderr.read().decode())  
+            stdout.channel.recv_exit_status()  # 等待命令执行完成
+            if stdout.channel.exit_status == 0:
+                write_log("通过SSH执行PM2命令成功")
+                print("温馨提醒：PM2进程恢复成功。")
+            else:
+                write_log(f"通过SSH执行PM2命令时出错，错误信息：{stderr.read().decode()}")  
+                print("警告：PM2进程恢复失败！\n错误信息：", stderr.read().decode())  
+        except Exception as e:  
+            write_log(f"通过SSH执行PM2命令时出错: {e}")  
+            print(f"通过SSH执行命令时出错: {e}")  
+    finally:  
+        ssh.close()  
+        transport.close()  
 
 # 尝试通过SSH连接的函数
 def ssh_connect():
     try:
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(**ssh_info)
-        
-        stdin, stdout, stderr = ssh.exec_command('ls -l')
-        output = stdout.read().decode()
-        
-        write_log("SSH连接成功")
-        print("SSH连接成功")
-        
-        ssh.close()
+        transport = paramiko.Transport((ssh_info['host'], ssh_info['port']))
+        transport.connect(username=ssh_info['username'], password=ssh_info['password'])
+        ssh_status = "SSH连接成功"
+        print("SSH连接成功。")
     except Exception as e:
-        write_log(f"SSH连接失败: {str(e)}")
-        print(f"SSH连接失败: {str(e)}")
+        ssh_status = f"SSH连接失败，错误信息: {e}"
+        print(f"SSH连接失败: {e}")
+    finally:
+        transport.close()
+        write_log(f"{ssh_status}")
 
 # 检查是否为每月的1号
 def is_first_day_of_month():
@@ -163,8 +145,8 @@ if __name__ == '__main__':
         print(f"解析成功，IP地址为: {host}")
         write_log(f"{host}")
 
-        # 尝试获取URL的状态码
-        response = requests.get(url, timeout=10)
+        # 尝试获取URL的状态码，忽略SSL证书验证
+        response = requests.get(url, timeout=10, verify=False)
         status_code = response.status_code
         if status_code != 200:
             # URL状态码不是200，发送通知并尝试恢复PM2进程
@@ -186,6 +168,16 @@ if __name__ == '__main__':
         message = f"💣 解析失败提醒 💣\n地址: {url}\n错误: {e}\n😱 抓紧尝试检查解析配置或联系管事的老铁。\n-------------------------------------\n系统时间: {system_time}\n北京时间: {beijing_time}"
         send_telegram_message(message)
         host = "解析失败"
+        status_code = "N/A"
+
+    except requests.RequestException as e:
+        # 请求异常，发送通知
+        write_log(f"请求失败: {e}")
+        system_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        beijing_time = datetime.now(pytz.timezone('Asia/Shanghai')).strftime('%Y-%m-%d %H:%M:%S')
+        message = f"🚨 请求失败 🚨\n地址: {url}\n错误: {e}\n-------------------------------------\n系统时间: {system_time}\n北京时间: {beijing_time}"
+        send_telegram_message(message)
+        host = "请求失败"
         status_code = "N/A"
 
     # 添加这些行
